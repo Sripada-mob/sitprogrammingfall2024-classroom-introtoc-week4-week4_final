6af01c729dda3e55b4c0a158d4ea7740cd460ed5
Sripada-mob <am24102@shibaura-it.ac.jp>
2024-10-27 10:32:13 +0900

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+
+    int x;
+
+    printf("Please input any number:");
+    scanf("%d", &x);
+
+    if(x>0) {
+            printf("The number is positive.\n");
+    } else {
+            printf("The number is negative or zero. \n");
+    }
+
+    return 0;
+}
