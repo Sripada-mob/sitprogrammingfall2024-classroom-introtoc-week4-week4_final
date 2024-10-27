16d852e3ca6082e21fe838cee403e7c62d7a6c82
Sripada-mob <am24102@shibaura-it.ac.jp>
2024-10-27 10:40:24 +0900

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+
+    float p,q,h;    
+    
+    printf("Please provide the probability of team A winninng:");
+    scanf("%f", &p);
+
+    q = 1-p;
+
+    h = (-p * (log(p)/log(2))) - (q * (log(q)/log(2)));
+
+    printf("The entropy rate of the match is %f \n", h);
+
+    return 0;
+}
