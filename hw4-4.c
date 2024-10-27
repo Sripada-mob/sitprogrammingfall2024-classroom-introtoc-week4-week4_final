7cab998b305e43c684298ca536c026618ad6dbcf
Sripada-mob <am24102@shibaura-it.ac.jp>
2024-10-27 10:36:21 +0900

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+
+    int x,y;
+
+    printf("Please provide any two numbers");
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    if (x>y) {
+            printf("The greater number of the two given numbers is %d \n", x);
+    } else {
+     printf("The greater number of the two given numbers is %d \n", y);   
+    }
+
+    return 0;
+}
