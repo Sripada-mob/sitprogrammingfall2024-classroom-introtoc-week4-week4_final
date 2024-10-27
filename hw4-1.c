ac8777c014824b9e79c0981053c904bd2df8eddc
Sripada-mob <am24102@shibaura-it.ac.jp>
2024-10-27 10:30:18 +0900

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(){
+    int x,y,z,mean;
+
+    printf("Please input any three numbers:");
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+
+    mean = (x+y+z)/3;
+
+    printf("The average of the given three values is %d \n", mean);
+
+    return 0;
+}
