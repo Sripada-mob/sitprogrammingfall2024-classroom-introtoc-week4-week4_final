3ed7552dec50c1f8f92fea9cb8595cce61f11058
Sripada-mob <am24102@shibaura-it.ac.jp>
2024-10-27 10:41:53 +0900

    Update hw4-5.c

--- hw4-5.c
+++ hw4-5.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
 
+int main(void) {
+
+    double userheight, userweight, ideal;
+    int diff;
+
+    printf("userweight? ");
+    scanf("%lf", &userweight);
+    
+    printf("userheight? ");
+    scanf("%lf", &userheight);
+
+    ideal = userheight * userheight * 22 / 10000;
+
+    printf("Ideal weight = %lf \n", ideal);
+
+    diff = userweight - ideal;
+
+    printf("diff = %f \n", diff);
+
+    
+    if(diff >= 5){
+            printf("Let's reduce sweets\n");
+                }
+    
+    else if(diff <= -5){ 
+        printf("Eat some more \n");
+        }
+    
+    else{
+        printf("Go for it");
+            }
+    
+    return 0;
+}
