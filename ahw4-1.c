fc4503aff089f0977379b4171342418c4568bb7e
Sripada-mob <am24102@shibaura-it.ac.jp>
2024-10-27 10:44:35 +0900

    Update ahw4-1.c

--- ahw4-1.c
+++ ahw4-1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+
+    int x;
+
+    printf("Please input any number:");
+    scanf("%d", &x);
+
+    if(x > 0) {
+            printf("%d is a positive number.\n", x);
+    } else if(x < 0) {
+            printf("%d is a negative number. \n", x);
+    } else {
+            printf("You entered %d\n", x);
+    }
+
+    return 0;
+}
