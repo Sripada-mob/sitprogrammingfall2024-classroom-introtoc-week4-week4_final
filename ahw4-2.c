14db591c10337346b5d0aa927888ee7982475d38
Sripada-mob <am24102@shibaura-it.ac.jp>
2024-10-29 13:28:59 +0900

    Update ahw4-2.c

--- ahw4-2.c
+++ ahw4-2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+
+    int x,y,z;
+
+    printf("Please provide any three numbers.\n");
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+
+    if(x > y) {
+            if(x > z) {
+                    printf("%d is the largest number\n", x);
+            } else {    
+                    printf("%d is the largest number\n", z);
+                    }
+    }else if(y > z) {
+            printf("%d is the largest numuber\n", y);
+    }else {
+            printf("%d is the largest numuber\n", z);
+    }
+
+    return 0;
+}
